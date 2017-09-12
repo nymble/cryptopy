c4248697722f3442ff2aa809b014322d25300e6b
nymble <nymble@gmail.com>
2017-09-12 13:37:51 -0700

    minor updates

--- ecc/ecc.py
+++ ecc/ecc.py
@@ -3,73 +3,85 @@
 
     Elliptic Curve Cryptograpy
 
-Refs:   Extension and repackaging of Peter Pearson's open source ECC
+    This module contains the base mathmatical operations for
+    the supported elliptic curves.
+
+    Specific elliptic curves are defined using these classes
+    in curves.py
+
+    Refs:
+        Peter Pearson's open source ECC
         http://safecurves.cr.yp.to/index.html
         http://ed25519.cr.yp.to/python/ed25519.py
         https://github.com/warner/python-ed25519/blob/master/kat.py
-      
 
-20131201 refactored
-2015  projections and new curve type changes
 
-Paul A. Lambert 2015
+    20131201 refactored
+    2015  projections and new curve type changes
+
+    Copyright Paul A. Lambert 2017
 """
 from ellipticcurve import EllipticCurveFp
-  
+from numbertheory import square_root_mod_prime
+
 
 class SmallWeierstrassCurveFp( EllipticCurveFp ):
     """ A Small Wierstrass curve has points on:
             y^2 == x^3 + a*x^2+b  over  GF(p)
     """
     def on_curve(curve, point):
-        p = curve.p;  a = curve.a;  b = curve.b;  x = point.x;  y = point.y
+        p = curve.p;  a = curve.a; b = curve.b;
+        x = point.x;  y = point.y; INFINITY = curve.IDENTITY
 
-        return  (y**2) % p == (x**3 + a*x + b) % p
-          
+        if point == INFINITY: # point at infinity
+            return True
+        else:
+            return  (y**2) % p == (x**3 + a*x + b) % p
+
     def add_points(curve, p1, p2):
         """ Add one point to another point (from X9.62 B.3). """
         assert p1.curve == p2.curve    # points must be on the same curve
-        INFINITY = curve.identity()
+        INFINITY = curve.IDENTITY
         if p2 == INFINITY:
             return p1
         if p1 == INFINITY:
             return p2
-        
+
         x1 = p1.x; y1 = p1.y; x2 = p2.x; y2 = p2.y; p = curve.p #for readability
         inv = curve.inverse
         if x1 == x2:
             if ( y1 + y2 ) % p == 0:
                 return INFINITY
             else:
-                return p1.double()      
-        l = ( (y2-y1) * inv( x2-x1) ) % p    
+                return p1.double()
+        l = ( (y2-y1) * inv( x2-x1) ) % p
         x3 = (l * l - x1 - x2) % p
-        y3 = (l * ( x1 - x3 ) - y1) % p       
+        y3 = (l * ( x1 - x3 ) - y1) % p
         return curve.point(x3, y3)
-        
+
     def negate(curve, point):
         """ Negate a point """
         return curve.point( point.x, -point.y )
 
     def double_point(curve, point):
-        """Return a new point that is twice the old (X9.62 B.3)."""   
+        """Return a new point that is twice the old (X9.62 B.3)."""
         p = curve.p; a = curve.a; inv = curve.inverse
         x = point.x; y = point.y
-        
-        l = ( (3*x*x + a) * inv(2*y) ) % p    
+
+        l = ( (3*x*x + a) * inv(2*y) ) % p
         x3 = (l*l - 2*x) % p
-        y3 = (l * (x - x3) - y) % p       
+        y3 = (l * (x - x3) - y) % p
         return curve.point(x3, y3)
-    
+
     def scalar_multiple(curve, point, scalar):
-        """Multiply a point by an integer (From X9.62 D.3.2). """ 
-        INFINITY = curve.identity()
+        """Multiply a point by an integer (From X9.62 D.3.2). """
+        INFINITY = curve.IDENTITY
         def leftmost_bit( x ):
             assert x > 0
             result = 1L
             while result <= x: result = 2 * result
             return result / 2
-    
+
         e = scalar
         e = e % point.curve.n
         if e == 0:
@@ -77,7 +89,7 @@ class SmallWeierstrassCurveFp( EllipticCurveFp ):
         if point == INFINITY:
             return INFINITY
         assert e > 0
-    
+
         e3 = 3 * e
         negative_self = curve.point( point.x, -point.y )
         i = leftmost_bit( e3 ) / 2
@@ -88,13 +100,27 @@ class SmallWeierstrassCurveFp( EllipticCurveFp ):
                 result = result + point
             if ( e3 & i ) == 0 and ( e & i ) != 0:
                 result = result + negative_self
-            # print ". . . i = %d, result = %s" % ( i, result )
-            i = i / 2   
+            i = i / 2
         return result
 
     def identity(curve):
-        """ The additive identity """
-        return curve.point(None, None) # Special point at infinity
+        """ The additive identity. Special values for 'infinity'
+            Note - idenity is required by 'on_curve' check so
+            validate is disabled for this special point.
+        """
+        return curve.point(None, None, validate=False)
+
+    @classmethod
+    def y_from_x(cls, x):
+        """ Returns one of the two possible values for y from x.
+            Used for point decompression.
+        """
+        a = cls.a; b = cls.b; p = cls.p
+        y_squared = ((x*x+ a)*x + b ) % p
+        # it might be y or p - y
+        y = square_root_mod_prime( y_squared, p )
+        return y
+
 
 
 class KoblitzCurveFp( SmallWeierstrassCurveFp ):
@@ -102,68 +128,67 @@ class KoblitzCurveFp( SmallWeierstrassCurveFp ):
             y**2 == x**3 + b  over  GF(p)
     """
     a = 0
-    
-    
+
+
 class TwistedEdwardsCurveFp( EllipticCurveFp ):
     """ A Twisted Edwards curve has points on:
             (a*x**2 + y**2) % p == (1 + d*x**2*y**2) % p
-    """       
+    """
     def on_curve(curve, g):
         """ Returns true if the point 'g' is on the curve """
         x = g.x; y = g.y; d = curve.d;  a = curve.a;  p = curve.p
 
-        return  (a*x**2 + y*y - 1 - d*x*x*y*y)  % p == 0
-        #xx = x*x
-        #return  (a*xx - 1 + y*(1 - d*xx*y))  % p == 0
-            
+        #return  (a*x**2 + y*y - 1 - d*x*x*y*y)  % p == 0
+        xx = x*x
+        return  (a*xx - 1 + y*(1 - d*xx*y))  % p == 0
+
     def add_points(curve, p1, p2):
         """ Add two points on the curve """
         x1 = p1.x;  x2 = p2.x;  y1 = p1.y;  y2 = p2.y
         d = curve.d; a = curve.a; p = curve.p; inv = curve.inverse
-        
-        # Edwards curve addition 
+
+        # Edwards curve addition
         x3 = ( (x1*y2+x2*y1) * inv(1+d*x1*x2*y1*y2) ) % p
         y3 = ( (y1*y2-a*x1*x2) * inv(1-d*x1*x2*y1*y2) ) % p    # ??? a=1 or a=-1 for Edwards?
         return curve.point(x3, y3)
-        
+
     def negate(curve, p):
         """ Negate a point """
         return curve.point(-p.x, p.y)
 
     def double_point(curve, point):
         """ Return a new point that is twice the old """
-        
         # Edwards curve point doubling
         return point+point
-            
+
         x = point.x; y = point.y; inv = curve.inverse
         # following hangs for n*G ???????
         x_sqrd = x*x;   y_sqrd = y*y
         x2 = (2*x*y)*inv(x_sqrd + y_sqrd)
         y2 = (y_sqrd - x_sqrd)*inv(2 - x_sqrd - y_sqrd)
         return curve.point(x2, y2)
-        
+
     def scalar_multiple(curve, point, scalar):
         if scalar == 0:
-            return curve.identity()
+            return curve.IDENTITY
         q = curve.scalarmult(point, scalar/2)
         q = q + q
         if scalar & 1:
             q = q + point
         return q
-        
+
     def identity(curve):
         """ The additive identity """
         return curve.point(0,-1)
-        
+
 class EdwardsCurveFp( TwistedEdwardsCurveFp ):
     """ An Edwards curve is a Twisted Edwards curve with a = 1
             (a*x**2+y**2)%p == (1+d*x**2*y**2)%p
-            or 
+            or
             (x**2+y**2)%p == (1+d*x**2*y**2)%p
     """
     a = 1
-     
+
 class MontgomeryCurveFp( EllipticCurveFp ):
     """ A Montogomery curve has points on:
             y**2 == x**3 + a*x**2 + x  modulo the prime p
@@ -171,17 +196,16 @@ class MontgomeryCurveFp( EllipticCurveFp ):
     def on_curve(curve, g):
         """ Is the point 'g' on the curve? """
         x = g.x; y = g.y; a = curve.a; p = curve.p; x_sqrd = x*x
-        
+
         return (y*y) % p == (x*x_sqrd + a*x_sqrd + x) % p
 
     def add_points(curve, p1, p2):
         """ Montgomery elliptic curve point addition"""
-        IDENTITY = curve.identity()
-        if p2 == IDENTITY:
+        if p2 == curve.IDENTITY:
             return p1
-        if p1 == IDENTITY:
+        if p1 == curve.IDENTITY:
             return p2
-       
+
         x1 = p1.x; x2 = p2.x; y1 = p1.y; y2 = p2.y
         a = curve.a; p = curve.p; inv = curve.inverse
 
@@ -191,8 +215,7 @@ class MontgomeryCurveFp( EllipticCurveFp ):
             else:
                 assert y1 + y2 == 0
                 return p1
-
-        l = (y2-y1)*inv(x2-x1)        
+        l = (y2-y1)*inv(x2-x1)
         x3 = l**2 - a - x1 - x2
         y3 = l*(x1 - x3) - y1
         return curve.point(x3 % p, y3 % p)
@@ -206,7 +229,7 @@ class MontgomeryCurveFp( EllipticCurveFp ):
         """
         x1 = point.x; y1 = point.y; a = curve.a; inv = curve.inverse
         p = curve.p
-        
+
         l = (3*x1*x1 + 2*a*x1 + 1)*inv(2*y1)
         x3 = l*l - a - 2*x1
         y3 = l*(x1 - x3) - y1
@@ -214,13 +237,13 @@ class MontgomeryCurveFp( EllipticCurveFp ):
 
     def scalar_multiple(curve, point, scalar):
         if scalar == 0:
-            return curve.identity()
+            return curve.IDENTITY
         q = curve.scalar_multiple(point, scalar/2)
         q = q + q
         if scalar & 1:
             q = q + point
         return q
-   
+
     def identity(curve):
         """ The additive identity """
         return curve.point(0,-1)
@@ -229,7 +252,7 @@ class MontgomeryCurveFp( EllipticCurveFp ):
         """ Scalar multiplication on a Montgomery curve
         """
         x1 = point.x; a = curve.a; p = curve.p
-        
+
         x2,z2,x3,z3 = 1, 0, point.x, 1
         for i in reversed(range(255)):
             bit = 1 & (scalar >> i)
@@ -240,7 +263,7 @@ class MontgomeryCurveFp( EllipticCurveFp ):
             x2, x3 = cswap(x2, x3, bit)
             z2, z3 = cswap(z2, z3, bit)
         return ( x2*z2**(p-2) ) % p
-        
+
     def montgomery_ladder2(curve, scalar, point):
         """
             1987 Montgomery "Speeding the Pollard and elliptic curve
@@ -248,7 +271,7 @@ class MontgomeryCurveFp( EllipticCurveFp ):
             plus common-subexpression elimination, plus assumption Z1=1.
         """
         x1 = point.x; a = curve.a; p = curve.p
-        a24 = (a - 2) / 4   
+        a24 = (a - 2) / 4
         x_2, z_2, x_3, z_3 = 0, 1, x_1, 1
         for t in reversed(range(255)):
             s_t = 1 & (scalar >> t)
@@ -262,9 +285,9 @@ class MontgomeryCurveFp( EllipticCurveFp ):
             C = x_3 + z_3
             D = x_3 - z_3
             DA = D * A
-            CB = C * B    
-            x_3 = (DA + CB)**2   
-            z_3 = x_1 * (DA - CB)**2  
+            CB = C * B
+            x_3 = (DA + CB)**2
+            z_3 = x_1 * (DA - CB)**2
             x_2 = AA * BB
             z_2 = E * (AA + a24 * E)   #BB?
             (x_2, x_3) = cswap (x_2, x_3, s_t)
@@ -277,7 +300,7 @@ def cswap(x2, x3, bit):
     x2 = x2 - dummy
     x3 = x3 + dummy
     return (x2, x3)
-    
+
 def int_to_string( x, padto=None ):
     """ Convert integer x into a string of bytes, as per X9.62.
         If 'padto' defined, result is zero padded to this length.
