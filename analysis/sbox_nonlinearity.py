ca7da0ddabed660643cf795070a5b9ad3a5a83b2
Nymble <nymble@gmail.com>
2019-02-11 17:04:26 -0800

    Add Streebog
    Added Streebog box from CFRG discussion 2/11/2019

--- analysis/sbox_nonlinearity.py
+++ analysis/sbox_nonlinearity.py
@@ -5,6 +5,8 @@
     Currently hardwired for AES and SMS4.
 
     Copyright (c) 2008 by Paul A. Lambert
+    
+    2019 - added Streebog sbox
 """
 
 def sboxNonlinearity(sbox):
@@ -139,6 +141,29 @@ if __name__ == "__main__":
            0x79,0xee,0x5f,0x3e,0xd7,0xcb,0x39,0x48 )
 
     print "sms4 non-linearity (min, max) = ", sboxNonlinearity(sms4_sbbytes)
+    
+     
+    # Streebog sbox from http://apmi.bsu.by/assets/files/std/belt-spec27.pdf
+    streebog_sbox_string = """
+        B1 94 BA C8 0A 08 F5 3B 36 6D 00 8E 58 4A 5D E4
+        85 04 FA 9D 1B B6 C7 AC 25 2E 72 C2 02 FD CE 0D
+        5B E3 D6 12 17 B9 61 81 FE 67 86 AD 71 6B 89 0B
+        5C B0 C0 FF 33 C3 56 B8 35 C4 05 AE D8 E0 7F 99
+        E1 2B DC 1A E2 82 57 EC 70 3F CC F0 95 EE 8D F1
+        C1 AB 76 38 9F E6 78 CA F7 C6 F8 60 D5 BB 9C 4F
+        F3 3C 65 7B 63 7C 30 6A DD 4E A7 79 9E B2 3D 31
+        3E 98 B5 6E 27 D3 BC CF 59 1E 18 1F 4C 5A B7 93
+        E9 DE E7 2C 8F 0C 0F A6 2D DB 49 F4 6F 73 96 47
+        06 07 53 16 ED 24 7A 37 39 CB A3 83 03 A9 8B F6
+        92 BD 9B 1C E5 D1 41 01 54 45 FB C9 5E 4D 0E F2
+        68 20 80 AA 22 7D 64 2F 26 87 F9 34 90 40 55 11
+        BE 32 97 13 43 FC 9A 48 A0 2A 88 5F 19 4B 09 A1
+        7E CD A4 D0 15 44 AF 8C A5 84 50 BF 66 D2 E8 8A
+        A2 D7 46 52 42 A8 DF B3 69 74 C5 51 EB 23 29 21
+        D4 EF D9 B4 3A 62 28 75 91 14 10 EA 77 6C DA 1D"""
+
+    streebog_sbbytes = [int(byte_chars,16) for byte_chars in streebog_sbox_string.split()]
+    print "Streebog non-linearity  (min, max) = ", sboxNonlinearity(streebog_sbbytes)
 
     # random sbox for comparison 
     from random import shuffle
